0a50babdd4b6238d0e84d05c1ef4b61c0dd3d355
Cy <cybunayog@gmail.com>
2020-02-06 14:28:43 -0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import EStylesheet from 'react-native-extended-stylesheet';
+import Home from './screens/Home';
+
+EStylesheet.build({
+  $primaryBlue: '#4F6D7A',
+  $white: '#FFFFFF',
+});
+
+export default () => <Home />;
