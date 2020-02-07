b4c92f4341751915035307ed5944d1210729277d
Cy <cybunayog@gmail.com>
2020-02-06 17:00:53 -0800

    Last Converted created

--- app/screens/Home.js
+++ app/screens/Home.js
@@ -5,11 +5,14 @@ import {Container} from '../components/Container';
 import {Logo} from '../components/Logo';
 import {InputWithButton} from '../components/TextInput';
 import {ClearButton} from '../components/Buttons';
+import {LastConverted} from '../components/Text';
 
 const TEMP_BASE_CURRENCY = 'USD';
 const TEMP_QUOTE_CURRENCY = 'GBP';
 const TEMP_BASE_PRICE = '100';
-const TEMP_QUOTE_PRICE = '79.74';
+const TEMP_QUOTE_PRICE = '77.34';
+const TEMP_CONVERSION_RATE = '0.7774';
+const TEMP_CONVERSION_DATE = new Date();
 
 class Home extends Component {
   handlePressBaseCurrency = () => {
@@ -31,11 +34,7 @@ class Home extends Component {
   render() {
     return (
       <Container>
-        <StatusBar
-          backgroundColor="blue"
-          translucent={false}
-          barStyle="light-content"
-        />
+        <StatusBar translucent={false} barStyle="light-content" />
         <Logo />
         <InputWithButton
           buttonText={TEMP_BASE_CURRENCY}
@@ -49,6 +48,12 @@ class Home extends Component {
           onPress={this.handlePressQuoteCurrency}
           editable={false}
           value={TEMP_QUOTE_PRICE}
+        />
+        <LastConverted
+          base={TEMP_BASE_CURRENCY}
+          quote={TEMP_QUOTE_CURRENCY}
+          date={TEMP_CONVERSION_DATE}
+          conversionRate={TEMP_CONVERSION_RATE}
         />
         <ClearButton
           text="Reverse Currencies"
